4655859223fdc3e20df08ec81f96b4d6f2bc95d2
Peter Monks <pmonks@gmail.com>
2016-02-16 17:33:23 -0800

    :white_check_mark: Added tests

---
+++ test/spinner/core_test.clj
@@ -0,0 +1,52 @@
+;
+; Copyright © 2016 Peter Monks (pmonks@gmail.com)
+;
+; All rights reserved. This program and the accompanying materials
+; are made available under the terms of the Eclipse Public License v1.0
+; which accompanies this distribution, and is available at
+; http://www.eclipse.org/legal/epl-v10.html
+;
+; Contributors:
+;    Peter Monks - initial implementation
+
+(ns spinner.core-test
+  (:require [clojure.java.io :as    io]
+            [midje.sweet     :refer :all]
+            [spinner.core    :refer :all]))
+
+(fact "Creation"
+  (create!) => truthy)
+
+(fact "Start and stop"
+  (let [s (create!)]
+    (do (start! s) (stop! s))) => nil?)
+
+(fact "Display"
+  (let [s (create!)]
+    (do (start! s) (Thread/sleep 1000) (stop! s))) => nil?)
+
+(fact "Custom colours"
+  (let [s (create! { :fg-colour :white :bg-colour :blue })]
+    (do (start! s) (Thread/sleep 1000) (stop! s))) => nil?)
+
+(fact "Custom characters"
+  (let [s (create! { :characters (:up-and-down styles) })]
+    (do (start! s) (Thread/sleep 1000) (stop! s))) => nil?)
+
+(fact "Print messages"
+  (do
+    (print "Reticulating splines... ")
+    (flush)
+    (let [s (create-and-start!)]
+      (Thread/sleep 500)
+      (print "\nInserting sublimated messages... ")
+      (Thread/sleep 500)
+      (print "\nAttempting to lock back buffer... ")
+      (Thread/sleep 500)
+      (print "\nTime-compressing simulator clock... ")
+      (Thread/sleep 500)
+      (print "\nLecturing errant subsystems... ")
+      (Thread/sleep 500)
+      (print "\nRetracting Phong shader... ")
+      (Thread/sleep 500)
+      (stop! s))) => nil?)
