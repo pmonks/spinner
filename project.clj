668a7dfd4d759e228596d04b14405faed882aff5
Peter Monks <pmonks@gmail.com>
2017-08-09 14:31:38 -0700

    :new: Add support for automated deployment of both JAR and docs via Travis CI

--- project.clj
+++ project.clj
@@ -10,18 +10,35 @@
 ;    Peter Monks - initial implementation
 
 (defproject org.clojars.pmonks/spinner "0.4.0-SNAPSHOT"
-  :description      "Simple text spinner for command line Clojure apps."
-  :url              "https://github.com/pmonks/spinner"
-  :license          {:name "Eclipse Public License"
-                     :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :min-lein-version "2.5.0"
-  :dependencies [
-                  [org.clojure/clojure "1.8.0"]
-                  [jansi-clj           "0.1.1-SNAPSHOT"]
-                ]
-  :profiles {:dev {:dependencies [
-                                   [midje          "1.8.3"]
-                                   [clj-ns-browser "1.3.1"]
-                                 ]
-                   :plugins      [[lein-midje "3.2"]]}   ; Don't remove this or travis-ci will assplode!
-             :uberjar {:aot :all}})
+  :description         "Simple text spinner for command line Clojure apps."
+  :url                 "https://github.com/pmonks/spinner"
+  :license             {:name "Eclipse Public License"
+                        :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :min-lein-version    "2.5.0"
+  :plugins             [
+                         [lein-codox "0.10.3"]
+                       ]
+  :dependencies        [
+                         [org.clojure/clojure "1.8.0"]
+                         [jansi-clj           "0.1.1-SNAPSHOT"]
+                       ]
+  :profiles            {:dev {:dependencies [
+                                              [midje          "1.8.3"]
+                                              [clj-ns-browser "1.3.1"]
+                                            ]
+                              :plugins      [[lein-midje "3.2"]]}   ; Don't remove this or travis-ci will assplode!
+                        :uberjar {:aot :all}}
+  :deploy-repositories [
+                         ["snapshots" {:url      "https://clojars.org/repo"
+                                       :username :env/clojars_username
+                                       :password :env/clojars_password}]
+                         ["releases"  {:url      "https://clojars.org/repo"
+                                       :username :env/clojars_username
+                                       :password :env/clojars_password}]
+                       ]
+  :codox               {
+                         :source-uri "https://github.com/pmonks/multigrep/blob/master/{filepath}#L{line}"
+;                         :source-uri "https://github.com/pmonks/multigrep/blob/{version}/{filepath}#L{line}"
+;                         :metadata   {:doc/format :markdown}
+                       }
+  )
