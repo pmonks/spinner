78c1bdb0b73c66501f63b83e50f720ad9264ac39
Peter Monks <pmonks@gmail.com>
2019-10-25 09:22:14 -0700

    :arrow_up: Upgrade dependencies

--- project.clj
+++ project.clj
@@ -18,16 +18,16 @@
   :min-lein-version    "2.8.1"
   :repositories        [["sonatype-snapshots" {:url "https://oss.sonatype.org/content/groups/public" :snapshots true}]
                         ["jitpack"            {:url "https://jitpack.io"}]]
-  :dependencies        [[org.clojure/clojure "1.10.0"]
+  :dependencies        [[org.clojure/clojure "1.10.1"]
                         [jansi-clj           "0.1.1"]]
   :profiles            {:dev  {:plugins      [[lein-licenses "0.2.2"]
-                                              [lein-codox    "0.10.4"]]}
+                                              [lein-codox    "0.10.7"]]}
                         :1.5  {:dependencies [[org.clojure/clojure "1.5.1"]]}
                         :1.6  {:dependencies [[org.clojure/clojure "1.6.0"]]}
                         :1.7  {:dependencies [[org.clojure/clojure "1.7.0"]]}
                         :1.8  {:dependencies [[org.clojure/clojure "1.8.0"]]}
                         :1.9  {:dependencies [[org.clojure/clojure "1.9.0"]]}
-                        :1.10 {:dependencies [[org.clojure/clojure "1.10.0"]]}}
+                        :1.10 {:dependencies [[org.clojure/clojure "1.10.1"]]}}
   :deploy-repositories [["snapshots" {:url      "https://clojars.org/repo"
                                       :username :env/clojars_username
                                       :password :env/clojars_password}]
