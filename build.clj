d5d95147e9f38487b60c258d7e8c1c15c5ca34e4
Peter Monks <pmonks@gmail.com>
2021-11-13 12:20:54 -0800

    :new: Add check-asf-policy build task

--- build.clj
+++ build.clj
@@ -101,6 +101,13 @@ clojure -A:deps -T:build help/doc"
       (set-opts)
       (lic/licenses)))
 
+(defn check-asf-policy
+  "Checks this project's dependencies' licenses against the ASF's 3rd party license policy (https://www.apache.org/legal/resolved.html)."
+  [opts]
+  (-> opts
+      (set-opts)
+      (lic/check-asf-policy)))
+
 (defn check-release
   "Check that a release can be done from the current directory."
   [opts]
