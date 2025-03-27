9f579609ef0b38b9096eb67cf41f97405c7aa1b1
Peter Monks <pmonks@gmail.com>
2025-03-27 11:44:51 -0700

    :art: Switch from atom to promise for signaling the polling thread to stop

---
+++ test/progress/test_utils.clj
@@ -0,0 +1,13 @@
+;
+; Copyright © 2025 Peter Monks
+;
+; This Source Code Form is subject to the terms of the Mozilla Public
+; License, v. 2.0. If a copy of the MPL was not distributed with this
+; file, You can obtain one at https://mozilla.org/MPL/2.0/.
+;
+; SPDX-License-Identifier: MPL-2.0
+;
+
+(ns progress.test-utils)
+
+(def skip-slow-tests? (boolean (System/getenv "GITHUB_CI")))
